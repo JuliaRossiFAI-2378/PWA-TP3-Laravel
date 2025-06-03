cd8edfc45183f029ae5c1bf055a621f3d617677e
Federico Lores FAI - 2948 <federico.lores@est.fi.uncoma.edu.ar>
2025-06-03 02:51:16 -0300

    css formulario

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts/base')
 @section('content')
-    <div class="container flex justify-center m-16">
-        <form action='{{route("createPost")}}' method="POST">
+    <div class="flex justify-center bg-[#3366CC] rounded-xl m-6 ">
+        <form action='{{route("createPost")}}' method="POST" class="w-full mx-6" >
             @csrf
             <input type="hidden" name="user" id="user" value="2">
             <div class="mt-3">
-                <label for='title' class=" text-sm font-medium text-gray-700 dark:text-gray-300">T&iacute;tulo:</label>
-                <input type='text' id='title' name='title' placeholder="Ingrese el t&iacute;tulo" class="ml-1 p-1 border-1 border-grey-300 rounded-sm"/>
+                <x-input-label for="title" :value="__('Titulo:')" />
+                <x-text-input id="title" class="block mt-1 w-full " type="text" name="title" placeholder="Ingrese el t&iacute;tulo"  required autofocus autocomplete="username" />
             </div>
             <div class="mt-3">
-                <label for='category' class=" text-sm font-medium text-gray-700 dark:text-gray-300">Categoria:</label>
-                <select name="category" id="category">
+                <x-input-label for="category" :value="__('Categoria:')" />
+                <select name="category" id="category" class="rounded-md" >
                     @if($categories != null)
                         @foreach ($categories as $item)
                             @if($item['name'] == 'LeoNeed')
@@ -23,10 +23,14 @@
                 </select>
             </div>
             <div class="mt-3">
-                <label for='content' class='block text-sm font-medium text-gray-700 dark:text-gray-300 '>Contenido:</label>
-                <textarea id='content' name='content' class='border-1 border-grey-300 rounded-sm p-1'></textarea>
+                <x-input-label for="content" :value="__('Contenido:')" />
+                <textarea id='content' name='content' class='border-2 rounded-md p-4 h-96 w-full '></textarea>
+            </div>  
+            <div class="flex items-center justify-end my-4">
+            <x-primary-button class="ms-3 bg-[#33CCBB] hover:bg-cyan-500">
+                {{ __('Enviar') }}
+            </x-primary-button>
             </div>
-            <button type="submit">Enviar</button>
         </form>
     </div>
 @endsection
